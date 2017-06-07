19c0ad97afa4c4450ec96ef367f2d7781ab9868d
Hulk914 <ayushthakur2009@gmail.com>
2017-06-07 11:08:31 +0530

    Routing final update done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
-//import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard.component'
-import { AppRoutingModule } from "./app-routing.module";
-
+import { FormsModule }   from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
-    /*RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])*/
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
